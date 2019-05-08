ad0c33e4ca0bd2dbe6e8043eb9b884dfebfee05b
polubis <polubis@wp.pl>
2019-05-08 17:14:14 +0200

    Add bucket mechanic

--- bookstore/src/app/app.component.ts
+++ bookstore/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { BucketService } from './services/BucketService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bookstore';
+
+  @HostListener('window:beforeunload', ['$event'])
+  handleSaveBucket() {
+    this.bucketService.saveBucketAsCookie();
+  }
+
+  constructor(private bucketService: BucketService) { }
+
+  ngOnInit() {
+    this.bucketService.bucket.subscribe(value => {
+      console.log(value);
+    });
+  }
 }
--- bookstore/src/app/services/BucketService.ts
+++ bookstore/src/app/services/BucketService.ts
@@ -4,16 +4,22 @@ import { ApiService } from './ApiService';
 import { Book } from '../models/entities/Book';
 import { take } from 'rxjs/operators';
 import { Bucket } from '../models/entities/Bucket';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BucketService {
 
-  bucket = new BehaviorSubject<Bucket>({});
+  constructor(private apiService: ApiService, private cookieService: CookieService) {
 
-  constructor(private apiService: ApiService) {
+  }
 
+  bucket = new BehaviorSubject<Bucket>(this.readBucketFromCookie());
+  private readBucketFromCookie(): Bucket {
+    const bucketAsString: string = this.cookieService.get('bucket');
+    console.log(bucketAsString);
+    return bucketAsString ? JSON.parse(bucketAsString) : {};
   }
 
   addBookToBucket(newBook: Book) {
@@ -28,6 +34,22 @@ export class BucketService {
       } else {
         this.bucket.next({ ...currBucket, [newBook.id]: { book: newBook, quantity: 1 }, });
       }
+    });
+  }
+
+  removeBookFromBucket(bookId: number) {
+    this.bucket.pipe(take(1)).subscribe((currBucket: Bucket) => {
+      const newBucket = { ...currBucket };
+      delete newBucket[bookId];
+
+      this.bucket.next(newBucket);
+    });
+  }
+
+  saveBucketAsCookie() {
+    this.bucket.pipe(take(1)).subscribe((currBucket: Bucket) => {
+      console.log('siema');
+      this.cookieService.set('bucket', JSON.stringify(currBucket));
     });
   }
 }
