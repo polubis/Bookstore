0dc51301a46a0baa48bee2083d28bf292057ef8d
polubis <polubis@wp.pl>
2019-05-09 23:58:13 +0200

    Add sub

--- bookstore/src/app/pages/admin/side-nav/side-nav.component.ts
+++ bookstore/src/app/pages/admin/side-nav/side-nav.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 const hightLightConfig = {
   '/admin': 0,
@@ -10,21 +12,25 @@ const hightLightConfig = {
   '/admin/orders': 4
 };
 
+@AutoUnsubscribe()
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent implements OnInit, OnDestroy {
   actualHightlightDistance = 0;
+  sub: Subscription;
 
   constructor(private router: Router) {
-    this.router.events.pipe(debounceTime(50)).subscribe(({ urlAfterRedirects }: NavigationEnd) => {
+    this.sub = this.router.events.pipe(debounceTime(50)).subscribe(({ urlAfterRedirects }: NavigationEnd) => {
         this.actualHightlightDistance = hightLightConfig[urlAfterRedirects];
     });
   }
 
   ngOnInit() {
   }
+
+  ngOnDestroy() {}
 
 }
